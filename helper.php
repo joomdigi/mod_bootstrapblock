4d014396b008e228b944aaa78f6fd386a4d6aa77
= <=>
2014-03-26 14:32:38 -0400

    first commit

---
+++ helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @version     1.0
+ * @package     mod_bootstrapblock
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @author      Brad Traversy <support@joomdigi.com> - http://www.joomdigi.com
+ */
+//No Direct Access
+defined('_JEXEC') or die;
+
+class modBootstrapBlockHelper{
+   
+}
+?>
